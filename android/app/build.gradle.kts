2b78d2053122064733689e23f283200b252b2412
Devrath <devrath@mplgaming.com>
2022-07-01 20:54:16 +0530

    added files

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -62,14 +62,8 @@ dependencies {
     implementation(DaggerHilt.hiltAndroid)
     kapt(DaggerHilt.hiltCompiler)
 
-   /*
-    implementation(project(Modules.coreUi))
-    implementation(project(Modules.onboardingPresentation))
-    implementation(project(Modules.onboardingDomain))
-    implementation(project(Modules.trackerPresentation))
-    implementation(project(Modules.trackerDomain))
-    implementation(project(Modules.trackerData))*/
     implementation(project(Modules.core))
+    implementation(project(Modules.coreUi))
     implementation(project(Modules.featuresSplashSplashPresentation))
     implementation(project(Modules.featuresSplashSplashDomain))
 
