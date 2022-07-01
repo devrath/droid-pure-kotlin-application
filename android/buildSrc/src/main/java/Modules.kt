658538ba8b608350c7b96a83f5fac203548c9ef2
Devrath <devrath@mplgaming.com>
2022-07-01 08:59:50 +0530

    added gradle files

---
+++ android/buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,13 @@
+object Modules {
+    const val app = ":app"
+
+    /*const val core = ":core"
+    const val coreUi = ":core-ui"
+
+    const val onboardingDomain = ":onboarding:onboarding_domain"
+    const val onboardingPresentation = ":onboarding:onboarding_presentation"
+
+    const val trackerData = ":tracker:tracker_data"
+    const val trackerDomain = ":tracker:tracker_domain"
+    const val trackerPresentation = ":tracker:tracker_presentation"*/
+}
---
+++ android/buildSrc/src/main/java/ProjectConfig.kt
@@ -0,0 +1,8 @@
+object ProjectConfig {
+    const val appId = "com.iprayforgod.app"
+    const val compileSdk = 31
+    const val minSdk = 21
+    const val targetSdk = 31
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
