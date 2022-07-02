68ba5ef3e6451fc13947d90a3ca52a25b03cb756
Devrath <devrath@mplgaming.com>
2022-07-02 13:49:48 +0530

    restructured themes

--- android/core-ui/build.gradle.kts
+++ android/core-ui/build.gradle.kts
@@ -3,5 +3,15 @@ apply {
 }
 
 dependencies {
+    "implementation"(Compose.compiler)
+    "implementation"(Compose.ui)
+    "implementation"(Compose.uiToolingPreview)
+    "implementation"(Compose.hiltNavigationCompose)
+    "implementation"(Compose.material)
+    "implementation"(Compose.runtime)
+    "implementation"(Compose.navigation)
+    "implementation"(Compose.viewModelCompose)
+    "implementation"(Compose.activityCompose)
 
+    "implementation"(Google.material)
 }
