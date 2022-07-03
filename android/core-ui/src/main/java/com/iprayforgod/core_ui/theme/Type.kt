861935b4c8d38fc57dd24af508af89b8790bef24
Devrath <devrath@mplgaming.com>
2022-07-03 20:57:28 +0530

    Added fonts

--- android/core-ui/src/main/java/com/iprayforgod/core_ui/theme/Type.kt
+++ android/core-ui/src/main/java/com/iprayforgod/core_ui/theme/Type.kt
@@ -2,49 +2,65 @@ package com.iprayforgod.core_ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.iprayforgod.core_ui.R
+
+
+val fonts = FontFamily(
+    Font(R.font.roboto_bold, weight = FontWeight.Bold),
+    Font(R.font.roboto_italic, weight = FontWeight.Normal, style = FontStyle.Italic),
+    Font(R.font.roboto_medium, weight = FontWeight.Medium, style = FontStyle.Normal),
+    Font(R.font.roboto_regular, weight = FontWeight.Normal, style = FontStyle.Normal)
+)
 
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     ),
     button = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
         fontWeight = FontWeight.W500,
         fontSize = 20.sp,
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp
     ),
     h1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
         fontWeight = FontWeight.Normal,
         fontSize = 30.sp
     ),
     h2 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
         fontWeight = FontWeight.Normal,
         fontSize = 26.sp
     ),
     h3 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp
     ),
     h4 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
         fontWeight = FontWeight.Normal,
         fontSize = 18.sp
     ),
     h5 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp
+    ),
+    h6 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
     ),
 )
