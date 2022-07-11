cc3f0124aa5809e4723d4ba9d8b02f4d0dcfd582
Devrath <devrath@mplgaming.com>
2022-07-11 20:33:26 +0530

    refactored gradle.kts

--- android/features/onboarding/onboarding_presentation/src/main/java/com/iprayforgod/view/OnBoardingScreen.kt
+++ android/features/onboarding/onboarding_presentation/src/main/java/com/iprayforgod/view/OnBoardingScreen.kt
@@ -4,9 +4,14 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.iprayforgod.vm.OnBoardingScreenVm
 
 @Composable
-fun OnBoardingScreen(){
+fun OnBoardingScreen(
+    //welcomeViewModel: OnBoardingScreenVm = hiltViewModel()
+){
+
     Column(
         modifier = Modifier
             .fillMaxSize(),
--- android/features/onboarding/onboarding_presentation/src/main/java/com/iprayforgod/vm/OnBoardingScreenVm.kt
+++ android/features/onboarding/onboarding_presentation/src/main/java/com/iprayforgod/vm/OnBoardingScreenVm.kt
@@ -1,12 +1,25 @@
 package com.iprayforgod.vm
 
+import androidx.lifecycle.viewModelScope
 import com.iprayforgod.core.base.BaseViewModel
+import com.iprayforgod.core.data.repositories.PreferenceRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class OnBoardingScreenVm @Inject constructor(
-
+    private val preferenceRepository : PreferenceRepository
 ) : BaseViewModel() {
+
+    /**
+     * Save the state of the on-boarding into the preferences via the repository
+     */
+    fun saveOnBoardingState(completed: Boolean) {
+        viewModelScope.launch(Dispatchers.IO) {
+            preferenceRepository.saveOnBoardingState(isOnBoardingShown = completed)
+        }
+    }
 
 }
