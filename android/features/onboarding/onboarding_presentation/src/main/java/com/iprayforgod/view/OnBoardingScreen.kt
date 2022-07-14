c49fb02a9952319516daf56a9a8115a2f69057ff
Devrath <devrath@mplgaming.com>
2022-07-14 17:21:14 +0530

    on boarding screen completed

--- android/features/onboarding/onboarding_presentation/src/main/java/com/iprayforgod/view/OnBoardingScreen.kt
+++ android/features/onboarding/onboarding_presentation/src/main/java/com/iprayforgod/view/OnBoardingScreen.kt
@@ -2,22 +2,13 @@
 
 package com.iprayforgod.view
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.accompanist.pager.*
 import com.iprayforgod.components.mainComponents.OnBoardingScreenContent
-import com.iprayforgod.components.subComponents.OnBoardingFinishButton
 import com.iprayforgod.components.subComponents.PagerData
 import com.iprayforgod.onboarding_domain.models.OnBoardingPageData
 import com.iprayforgod.onboarding_presentation.R
@@ -27,33 +18,8 @@ import com.iprayforgod.vm.OnBoardingScreenVm
 fun OnBoardingScreen(
     viewModel: OnBoardingScreenVm = hiltViewModel()
 ){
-    val pagerState = rememberPagerState()
-
-    Column(modifier = Modifier.fillMaxSize()) {
-        HorizontalPager(
-            modifier = Modifier.weight(10f),
-            count = 3,
-            state = pagerState,
-            verticalAlignment = Alignment.Top
-        ) { position ->
-            val pageData = viewModel.onBoardingPageData[position]
-            PagerData(data = pageData)
-        }
-        HorizontalPagerIndicator(
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .weight(1f),
-            pagerState = pagerState
-        )
-        OnBoardingFinishButton(
-            modifier = Modifier.weight(1f),
-            pagerState = pagerState
-        ){
-            viewModel.saveOnBoardingState(completed = true)
-            /* navController.popBackStack()
-            navController.navigate(Screen.Home.route)*/
-            // ---------------------------------------------> Perform some action
-        }
+    OnBoardingScreenContent(viewModel.onBoardingPageData){
+        viewModel.saveOnBoardingState(completed = true)
     }
 }
 
