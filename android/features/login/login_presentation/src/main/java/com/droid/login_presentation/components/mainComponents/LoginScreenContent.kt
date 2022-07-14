0b3e253a262bcca3f965f676901e2db2a4138769
Devrath <devrath@mplgaming.com>
2022-07-14 22:30:18 +0530

    Added login screen

---
+++ android/features/login/login_presentation/src/main/java/com/droid/login_presentation/components/mainComponents/LoginScreenContent.kt
@@ -0,0 +1,135 @@
+package com.droid.login_presentation.components.mainComponents
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun LoginScreenContent() { LoginPageContent() }
+
+@Composable
+fun LoginPageContent() {
+    Surface(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Gray)) {
+
+        val userName = remember { mutableStateOf(TextFieldValue())}
+        val password = remember { mutableStateOf(TextFieldValue())}
+
+        Box(modifier = Modifier.fillMaxSize()) {
+            ClickableText(
+                text = AnnotatedString("Sign up here"),
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(20.dp),
+                onClick = { },
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    fontFamily = FontFamily.Default,
+                    textDecoration = TextDecoration.Underline,
+                    color = Color.Black
+                )
+            )
+        }
+
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Text(
+                text = "Login",
+                style = TextStyle(
+                    fontSize = 40.sp,
+                    fontFamily = FontFamily.Monospace
+                )
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            TextField(
+                label = { Text(text = "Username") },
+                value = userName.value,
+                onValueChange = { userName.value = it },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            TextField(
+                label = { Text(text = "Password") },
+                value = password.value,
+                onValueChange = { password.value = it },
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
+                Button(
+                    onClick = { },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(50.dp),
+                    shape = RoundedCornerShape(50.dp)
+                ) {
+                    Text(
+                        text = "Login",
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+           Row(
+               horizontalArrangement = Arrangement.Center,
+               verticalAlignment = Alignment.CenterVertically
+           ) {
+
+               ClickableText(
+                   text = AnnotatedString("Forgot password"),
+                   onClick = {},
+                   style = TextStyle(
+                       fontSize = 14.sp,
+                       fontFamily = FontFamily.Default
+                   )
+               )
+           }
+
+        }
+
+    }
+}
+
+
+
+
+@Composable
+@Preview(showBackground = true)
+fun LoginPagePreview() { LoginPageContent() }
--- android/features/login/login_presentation/src/main/java/com/droid/login_presentation/view/LoginScreen.kt
+++ android/features/login/login_presentation/src/main/java/com/droid/login_presentation/view/LoginScreen.kt
@@ -1,26 +1,26 @@
 package com.droid.login_presentation.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.droid.login_presentation.components.mainComponents.LoginScreenContent
 import com.droid.login_presentation.vm.LoginVm
 
 @Composable
 fun LoginScreen(
     viewModel: LoginVm = hiltViewModel()
 ){
+   LoginScreenContent()
+}
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Gray),
-    ) {
-
-    }
-
-
+@Composable
+@Preview(showBackground = true)
+fun LoginScreenContentPreview() {
+    LoginScreenContent()
 }
