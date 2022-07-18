7cdf9cb9a221d236a6ad4972faccaebde18d3ec0
Devrath <devrath@mplgaming.com>
2022-07-18 10:49:27 +0530

    Added the flavours

--- android/buildSrc/src/main/java/ProjectConfig.kt
+++ android/buildSrc/src/main/java/ProjectConfig.kt
@@ -1,8 +1,11 @@
 object ProjectConfig {
-    const val appId = "com.com.iprayforgod.app"
+    const val appId = "com.iprayforgod.app"
     const val compileSdk = 31
     const val minSdk = 21
     const val targetSdk = 31
     const val versionCode = 1
     const val versionName = "1.0"
+    const val jvmTarget = "1.8"
+    const val testRunner = "com.iprayforgod.app.TestRunner"
+
 }
