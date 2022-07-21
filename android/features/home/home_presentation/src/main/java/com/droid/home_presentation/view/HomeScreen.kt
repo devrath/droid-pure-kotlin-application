1d8938324eb6810f04dd389dcacdee80bd0b4008
Devrath <devrath@mplgaming.com>
2022-07-21 20:50:45 +0530

    Added the drawer

--- android/features/home/home_presentation/src/main/java/com/droid/home_presentation/view/HomeScreen.kt
+++ android/features/home/home_presentation/src/main/java/com/droid/home_presentation/view/HomeScreen.kt
@@ -1,11 +1,21 @@
 package com.droid.home_presentation.view
 
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.droid.home_presentation.components.subComponents.ApplicationBar
+import com.droid.home_presentation.components.subComponents.DrawerBody
+import com.droid.home_presentation.components.subComponents.DrawerHeader
+import com.droid.home_presentation.components.subComponents.MenuItem
 import com.droid.home_presentation.vm.HomeVm
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeScreen(
@@ -14,8 +24,49 @@ fun HomeScreen(
 
     val context = LocalContext.current
     val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
 
-    Scaffold(scaffoldState = scaffoldState) {
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            ApplicationBar(
+                onNavigationIconClick = {
+                    scope.launch {
+                        scaffoldState.drawerState.open()
+                    }
+                }
+            )
+        },
+        drawerGesturesEnabled = scaffoldState.drawerState.isOpen,
+        drawerContent = {
+            DrawerHeader()
+            DrawerBody(
+                items = listOf(
+                    MenuItem(
+                        id = "home",
+                        title = "Home",
+                        contentDescription = "Go to home screen",
+                        icon = Icons.Default.Home
+                    ),
+                    MenuItem(
+                        id = "settings",
+                        title = "Settings",
+                        contentDescription = "Go to settings screen",
+                        icon = Icons.Default.Settings
+                    ),
+                    MenuItem(
+                        id = "help",
+                        title = "Help",
+                        contentDescription = "Get help",
+                        icon = Icons.Default.Info
+                    ),
+                ),
+                onItemClick = {
+                    println("Clicked on ${it.title}")
+                }
+            )
+        }
+    ) {
 
     }
 
