3348a47236ff4fe1130616014d61bc715f30a35f
Devrath <devrath@mplgaming.com>
2022-07-22 20:21:15 +0530

    added network bound resource

--- android/buildSrc/src/main/java/Build.kt
+++ android/buildSrc/src/main/java/Build.kt
@@ -3,6 +3,7 @@ object Build {
     const val androidBuildTools = "com.android.tools.build:gradle:$androidBuildToolsVersion"
 
     const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Kotlin.version}"
+    const val kotlinStdlibPlugin = "org.jetbrains.kotlin:kotlin-stdlib:${Kotlin.version}"
 
     private const val hiltAndroidGradlePluginVersion = "2.42"
     const val hiltAndroidGradlePlugin = "com.google.dagger:hilt-android-gradle-plugin:$hiltAndroidGradlePluginVersion"
