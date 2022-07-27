3da058971a44a3fea8951febdfb3a15357286953
Devrath <devrath@mplgaming.com>
2022-07-27 23:36:03 +0530

    v

--- android/features/login/login_presentation/src/main/java/com/droid/login_presentation/states/RegistrationUiEvent.kt
+++
@@ -1,9 +0,0 @@
-package com.droid.login_presentation.states
-
-import com.iprayforgod.core.platform.ui.uiEvent.UiText
-
-sealed class RegistrationUiEvent {
-    object Success : RegistrationUiEvent()
-    data class LoaderState(val isLoading: Boolean) : RegistrationUiEvent()
-    data class ShowSnackBar(val message: UiText) : RegistrationUiEvent()
-}
