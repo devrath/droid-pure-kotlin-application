b9c9abf4c36a9e00994594d426f8f6f099ff8c19
Devrath <devrath@mplgaming.com>
2022-08-01 15:30:23 +0530

    added dependencies

---
+++ android/features/login/login_domain/src/test/java/com/droid/login_domain/usecases/cases/forgotPassword/ValidateForgotPwdUseCaseTest.kt
@@ -0,0 +1,8 @@
+package com.droid.login_domain.usecases.cases.forgotPassword
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+
+@ExperimentalCoroutinesApi
+class ValidateForgotPwdUseCaseTest {
+
+}
