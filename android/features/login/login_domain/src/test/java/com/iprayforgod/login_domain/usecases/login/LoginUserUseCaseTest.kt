f01061fe863550d7c714688ff1cd893f4fd08daf
Devrath <devrath@mplgaming.com>
2022-08-04 12:37:37 +0530

    unit testing

--- android/features/login/login_domain/src/test/java/com/iprayforgod/login_domain/usecases/login/LoginUserUseCaseTest.kt
+++ android/features/login/login_domain/src/test/java/com/iprayforgod/login_domain/usecases/login/LoginUserUseCaseTest.kt
@@ -5,11 +5,16 @@ import com.droid.core_mock.core.domain.models.UserMocks
 import com.droid.core_mock.core.domain.models.UserMocks.MockCredentials.InvalidCredentials.IN_VALID_EMAIL
 import com.droid.core_mock.core.domain.models.UserMocks.MockCredentials.ValidCredentials.VALID_EMAIL
 import com.droid.core_mock.features.login.login_domain_mock.usecases.repository.FakeFailureLoginRepository
+import com.droid.core_mock.features.login.login_domain_mock.usecases.repository.FakeFailureMsgCheckLoginRepository
+import com.droid.core_mock.features.login.login_domain_mock.usecases.repository.FakeFailureMsgCheckLoginRepository.Companion.FAILURE_MESSAGE_FOR_LOGIN
 import com.droid.core_mock.features.login.login_domain_mock.usecases.repository.FakeSuccessLoginRepository
 import com.google.common.truth.Truth.assertThat
+import com.iprayforgod.core.domain.models.User
 import com.iprayforgod.core.platform.functional.State
 import com.iprayforgod.login_domain.utils.UseCaseUtilities
+import io.mockk.every
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
@@ -56,6 +61,28 @@ class LoginUserUseCaseTest {
 
         // ASSERT
         assertThat(result).isNotEqualTo(State.success(UserMocks.validUser()))
+    }
+
+
+
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `test repository for login failure message`() = runTest {
+        // ARRANGE
+        val emailInput = VALID_EMAIL
+        val passwordInput = "Hello!2345"
+        val fakeResource = UseCaseUtilities.prepareLoginInput(
+            email = emailInput, password = passwordInput
+        )
+        val fakeRepository = FakeFailureMsgCheckLoginRepository() // we use a fake repository that returns fake message
+
+        // ACT
+        val useCase = LoginUserUseCase(fakeRepository)
+        val result  = useCase.invoke(fakeResource).first()
+
+        // ASSERT
+        assertThat(result).isEqualTo(State.Failed<String>(message = FAILURE_MESSAGE_FOR_LOGIN))
     }
 
 
